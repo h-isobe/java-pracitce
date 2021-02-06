15881f924be8516a9056c9732841dec8a1ba86e0
磯部浩樹 <h.sep23@gmail.com>
2021-02-06 16:50:58 +0900

    [Add]chapter6-10

---
+++ sukkiri_java/chapter6-10/ApiSample.java
@@ -0,0 +1,11 @@
+public class ApiSample {
+  public static void main(String[] args) {
+
+    //コード6-10 API利用の例
+    int[] heights = {172, 149, 152, 191, 155};
+    java.util.Arrays.sort(heights);
+    for (int h : heights) {
+      System.out.println(h);
+    }
+  }
+}
