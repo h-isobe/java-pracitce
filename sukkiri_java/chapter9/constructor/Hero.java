8b7bff9a009ff54f7c171a7f1276fbc4afdf9ab8
磯部浩樹 <h.sep23@gmail.com>
2021-02-10 18:16:49 +0900

    [Update]constructor

--- sukkiri_java/chapter9/constructor/Hero.java
+++ sukkiri_java/chapter9/constructor/Hero.java
@@ -1,3 +1,11 @@
+//コンストラクタ
+//条件=>①メソッド名がクラス名と完全に等しい ②メソッド宣言に戻り値が記述されていない(voidもダメ)
 public class Hero {
-  
+  String name;
+  int hp;
+
+  public Hero(String name) {
+    this.hp = 100;     //hpフィールドを100で初期化
+    this.name = name;  //引数の値でnameフィールドを初期化
+  }
 }
