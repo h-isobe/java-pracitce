a886b0719901fac848712b5c262f0a186b9b4223
磯部浩樹 <h.sep23@gmail.com>
2021-02-11 16:36:17 +0900

    [Fix]階層

---
+++ sukkiri_java/chapter10/inheritance/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    SuperHero sh = new SuperHero();
+
+    Matango m = new Matango();
+    sh.attack(m);
+    sh.fly();
+    sh.run();
+  }
+}
