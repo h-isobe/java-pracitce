1f176ce8260f64577043cdd76e6591a83013b8e0
磯部浩樹 <h.sep23@gmail.com>
2021-02-19 18:04:07 +0900

    [Add]練習問題

---
+++ sukkiri_java/chapter16/practice16-3/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Hero h1 = new Hero("斎藤");
+    Hero h2 = new Hero("鈴木");
+    Map<Hero, Integer> heros = new HashMap<Hero, Integer>();
+    heros.put(h1, 3);
+    heros.put(h2, 7);
+    for (Hero key : heros.keySet()) {
+      int value = heros.get(key);
+      System.out.println(key.getName() + "が倒した敵=" + value);
+    }
+  }
+}
