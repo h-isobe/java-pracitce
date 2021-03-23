d1f5d6ab5b421a9ecd976bba998070be69818fc5
磯部浩樹 <h.sep23@gmail.com>
2021-03-23 16:19:37 +0900

    [Update]Cランクレベルアップメニュー_for文

--- paiza/c_rank_level_up_menu2.java
+++ paiza/c_rank_level_up_menu2.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class c_rank_level_up_menu2 {
   public static void main(String[] args) {
@@ -21,6 +22,20 @@ public class c_rank_level_up_menu2 {
     System.out.println(count);
 
 
+    //STEP: 2 フラグ管理
+    System.out.println("STEP: 2 フラグ管理");
+    Scanner sc2 = new Scanner(System.in);
+    int num2 = sc2.nextInt();
+    Integer[] array2 = new Integer[num2];
+    for(int i = 0; i < array2.length; i++) {
+        array2[i] = sc2.nextInt();
+    }
+    if(Arrays.asList(array2).contains(7)) {
+        System.out.println("YES");
+    } else {
+        System.out.println("NO");
+    }
+    
   }
 }
 
