87d8de9b6aa29c0d0685b2842d1ab80b87cf38d3
磯部浩樹 <h.sep23@gmail.com>
2021-03-23 18:29:24 +0900

    [Update]Cランクレベルアップメニュー

--- paiza/c_rank_level_up_menu2.java
+++ paiza/c_rank_level_up_menu2.java
@@ -35,6 +35,29 @@ public class c_rank_level_up_menu2 {
     } else {
         System.out.println("NO");
     }
+
+
+    //STEP: 3 インデックス取得
+    System.out.println("STEP: 3 インデックス取得");
+    Scanner sc = new Scanner(System.in);
+    //num人
+    int num = sc.nextInt();
+    //空のnum人分の配列
+    int[] array = new int[num];
+    //配列人数分の各自の財産を入力
+    for(int i = 0; i < array.length; i++) {
+        array[i] = sc.nextInt();
+    }
+    //財産k円
+    int k = sc.nextInt();
+    //配列から財産kと一致する配列番号を取得
+    //indexOfを使うためArraysListクラスからListを生成
+    //作成済みのarray配列の要素を1つずつ空のリスト配列に格納
+    List<Integer> list = new ArrayList<Integer>();
+    for(int i = 0; i < array.length; i++) {
+        list.add(array[i]);
+    }
+    System.out.println(list.indexOf(k) + 1);
     
   }
 }
