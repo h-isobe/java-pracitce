9ab8f07ec64c940ea420dece710948a6698d892c
磯部浩樹 <h.sep23@gmail.com>
2021-03-28 20:10:06 +0900

    [Update]二重ループメニュー

--- paiza/double_loop_menu/Main.java
+++ paiza/double_loop_menu/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -24,5 +25,61 @@ public class Main {
     for(int i = 1; i <= n; i++) {
       System.out.println(i);
     }
+
+
+    //STEP: 3 指定された行数の数字の出力
+    System.out.println("STEP: 3 指定された行数の数字の出力");
+    Scanner sc3 = new Scanner(System.in);
+    int n3 = sc3.nextInt();
+    for(int i = 0; i < n3; i++) {
+      for(int j = 1; j <= 5; j++) {
+        if(j < 5) {
+          System.out.print(j);
+          System.out.print(" ");  
+        } else {
+          System.out.println(j);
+        }
+      }
+    }
+
+
+    //STEP: 4 指定された範囲・行数の数字の出力
+    System.out.println("STEP: 4 指定された範囲・行数の数字の出力");
+    Scanner sc4 = new Scanner(System.in);
+    int[] array4 = Arrays.stream(sc4.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();  //標準入力 5 8 (1から5までの8行)
+    //1から5までの数字を8行分出力
+    for(int i = 1; i <= array4[1]; i++) {    //8行分
+      for(int j = 1; j <= array4[0]; j++) {  //1から5を
+        if(j < array4[0]) {
+          System.out.print(j);
+          System.out.print(" ");
+        } else {
+          System.out.println(j);
+        }
+      }    
+    }
+
+
+    //STEP: 5 二次元配列の出力
+    System.out.println("STEP: 5 二次元配列の出力");
+    Scanner sc5 = new Scanner(System.in);
+    int[] array5 = Arrays.stream(sc5.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();  //3行 4列
+    int[] arr5 = new int[array5[1]];
+    int count5 = 0;
+    for(int i = 0; i < array5[0]; i++) {  //3行分
+      for(int j = 0; j < array5[1]; j++) {
+        arr5[j] = sc5.nextInt();
+      }
+      for(int a5 : arr5) {
+        if(count5 < array5[1]) {
+            System.out.print(a5);
+            System.out.print(" ");
+        } else {
+            System.out.println(a5);
+        }
+      }
+      System.out.println(" ");
+    }
+
   }
 }
